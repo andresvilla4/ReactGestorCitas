5d22ddfe65a1fded9046f057929dbdf12d6f6f92
Arrogante <andresmauriciovillacobgarrido@hotmail.com>
2021-06-09 15:00:59 -0500

    Creando las citas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import Formulario from './components/Formulario';
 
 function App() {
+	
+	// Arreglo de citas
+	const [citas, guardarCitas] = useState([]);
+	
+	// Función que tome las citas actuales y agregue la nueva
+	const creaCita = cita => {
+		guardarCitas([
+			...citas,
+			cita
+		]);
+		}
+	
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
       <div className = "Container">
         <div row="row">
           <div className="one-half column">
-            <Formulario />
+            <Formulario 
+				creaCita={crearCita}
+            />
           </div>
           <div className="one-half column">
             2
